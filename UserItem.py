a2c3cdc9a4968c7147dfcd0aa945f9167b4455e1
di-press <diany_press@usp.br>
2021-05-26 00:37:35 -0300

    fix UserItem attributes

--- UserItem.py
+++ UserItem.py
@@ -1,4 +1,11 @@
 class UserItem:
 
-    def __init__(self, hash_id, movies, ratings, big_five):
-        pass
+    def __init__(self, hash_id, movies, big_five):
+        
+        self.hash_id = hash_id
+        self.movies = movies
+        self.extroversion = big_five['extroversion']
+        self.neuroticism = big_five['neuroticism']
+        self.agreeableness = big_five['agreeableness']
+        self.conscientiousness = big_five['conscientiousness']
+        self.openess = big_five['openess']

b043ca967df2faa22a578c5a2c4a24e00c29ca92
di-press <diany_press@usp.br>
2021-06-29 17:52:50 -0300

    almost completing title search

--- movielens_id_title.py
+++ movielens_id_title.py
@@ -1,33 +1,65 @@
 #maps the movielens id with title
+from os import name
 from pathlib import Path
 import pandas as pd
 
-movie_csv_file = Path.cwd().joinpath('movielens','ml-latest', 'movies.csv')
+def create_df():
 
-df = pd.read_csv (movie_csv_file,
-                    sep=",", warn_bad_lines=True, 
-                    error_bad_lines=True,
-                    engine='python',
-                    header=0,
-                    usecols = ['movieId',
-                                'title',
-                                'genres'] 
+    movie_csv_file = Path.cwd().joinpath('movielens','ml-latest', 'movies.csv')
 
-                )
+    df = pd.read_csv (movie_csv_file,
+                        sep=",", warn_bad_lines=True, 
+                        error_bad_lines=True,
+                        engine='python',
+                        header=0,
+                        usecols = ['movieId',
+                                    'title',
+                                    'genres'] 
 
-# 58098 movies
-#print(len(df))
+                    )
 
-find = 24
-#print(df['movieId'][find])
-
-found_object = df[df["movieId"] == find]
-# estudar pandas series pra poder 
-# converter o titulo numa string:
-print(found_object['title'])
+    # returns a deep copy; the original df won't never be modified
+    # df contains 58098 movies
+    return df.copy(deep=True)
 
 
+def find_title_by_movielens_id(movielens_id, df):
 
-#def map_movielens_id_and_title(movielens_id):
+    find = movielens_id
+    #print(df['movieId'][find])
+
+    found_object = df[df["movieId"] == find]
+    movie_title = found_object['title'].item()
 
 
+    print(movie_title)
+
+def find_titles(movielens_ids):
+
+    movie_csv_file = Path.cwd().joinpath('movielens','ml-latest', 'movies.csv')
+
+    df = pd.read_csv (movie_csv_file,
+                        sep=",", warn_bad_lines=True, 
+                        error_bad_lines=True,
+                        engine='python',
+                        header=0,
+                        usecols = ['movieId',
+                                    'title',
+                                    'genres'] 
+
+                    )
+
+    for id in movielens_ids:
+        
+        find = id
+        found_object = df[df["movieId"] == find]
+        movie_title = found_object['title'].item()
+
+
+        print(movie_title)
+
+if __name__ == "__main__":
+
+    movielens_ids = [24]
+    
+    find_titles(movielens_ids)

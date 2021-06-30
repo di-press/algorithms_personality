50c78df9cda2e383c1fb9646dff64364965fabe7
di-press <diany_press@usp.br>
2021-06-30 00:52:09 -0300

    starting find plot

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ https://grouplens.org/datasets/personality-2018/
 extract, and put it inside this project folder.
 
 Download latest movielens dataset, extract it and put inside the project folder.
+
+Download archive from kaggle (wiki_movie_plots_deduped.csv)
--- title_and_plots.py
+++ title_and_plots.py
@@ -2,22 +2,41 @@ from pathlib import Path
 import pandas as pd
 
 
-wiki_plots = Path.cwd().joinpath('archive', 'wiki_movie_plots_deduped.csv')
+def create_df():
 
-wiki_plots_df = pd.read_csv (wiki_plots,
-                    sep=",", warn_bad_lines=True, 
-                    error_bad_lines=True,
-                    engine='python',
-                    header=0,
-                    usecols = ['Release Year',
-                               'Title',
-                               'Origin/Ethnicity',
-                               'Director',
-                               'Cast',
-                               'Genre',
-                               'Wiki Page',
-                               'Plot']
-                               
-                    )
+    wiki_plots = Path.cwd().joinpath('archive', 'wiki_movie_plots_deduped.csv')
 
-print(wiki_plots_df)
+    wiki_plots_df = pd.read_csv (wiki_plots,
+                        sep=",", warn_bad_lines=True, 
+                        error_bad_lines=True,
+                        engine='python',
+                        header=0,
+                        usecols = ['Release Year',
+                                'Title',
+                                'Origin/Ethnicity',
+                                'Director',
+                                'Cast',
+                                'Genre',
+                                'Wiki Page',
+                                'Plot']
+                                
+                        )
+
+    
+    return wiki_plots_df.copy(deep=True)
+
+
+def find_plot(df, movie_title, year):
+
+        #found_object = df[df["Title"] == movie_title and df['Release Year'] == year]
+        found_object = df.query('Title == @movie_title & Release Year == @year')
+        if not found_object.empty:
+            plot = str(found_object['Plot'].item())
+            print(plot)
+
+if __name__ == '__main__':
+
+    df = create_df()
+    find_plot(df, 'My Girl', '1991')
+
+

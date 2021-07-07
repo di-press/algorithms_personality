00697bf8720f588d724d3b67df21ac9f8f7d130a
di-press <diany_press@usp.br>
2021-07-06 23:34:19 -0300

    correct string size and include tt in id

--- map_movielens_tmdb_id.py
+++ map_movielens_tmdb_id.py
@@ -28,7 +28,7 @@ def find_tmdb_id(movielens_ids, link_df):
 
     for id in movielens_ids:
         print("buscando id: ", id)
-        id = int(id)
+        #id = int(id)
         find = id
         #print(df['movieId'][find])
 
@@ -39,7 +39,19 @@ def find_tmdb_id(movielens_ids, link_df):
         
             movie_imdb_id = found_object['imdbId'].item()
             movie_imdb_id = str(movie_imdb_id)
+            
+            if(len(movie_imdb_id) < 7):
 
+                complementary_zeros = 7 - len(movie_imdb_id)
+
+                fill_zeros = ''
+
+                for _ in range(complementary_zeros):
+                    fill_zeros += '0'
+
+                movie_imdb_id = fill_zeros + movie_imdb_id
+
+            movie_imdb_id = 'tt' + movie_imdb_id
             # id is a number
             id_title_tuples.append((id, movie_imdb_id))
 
@@ -52,6 +64,6 @@ if __name__ == '__main__':
 
     link_df = create_link_df()
     #print(df)
-    movielens_ids = [122912]
+    movielens_ids = [122900, 1, 149300]
     found_objects = find_tmdb_id(movielens_ids, link_df)
     print(found_objects)

b2344bd76a325e722f75583520460b5e8def25a2
di-press <diany_press@usp.br>
2021-08-11 19:27:33 -0300

    almost finishing final dataset

--- Movie.py
+++ Movie.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class Movie:
     #big_five is a dict
-    def __init__(self, movielens_id):
+    def __init__(self, movielens_id, user_rating):
 
         movies_csv = Path.cwd().joinpath('movie_personality_and_genres.csv')
 
@@ -71,8 +71,6 @@ class Movie:
         self.genre = []
 
         if not found_object.empty:
-            print(found_object['extroversion'])
-
         
             self.movielens_id = movielens_id
             self.imdb_id = found_object['imdbId'].item()
@@ -81,33 +79,42 @@ class Movie:
             self.agreeableness = found_object['agreeableness'].item()
             self.conscientiousness = found_object['conscientiousness'].item()
             self.openess = found_object['openess'].item()
+            self.rating = user_rating
             
 
             for genre in movie_genres:
                 self.genre.append(found_object[genre].item())
 
-       
+        else:
+            self.movielens_id = "INVALID"
 
 
-def print_movie(self):
+    def print_movie(self):
 
-    print("movielensId: ", self.movielens_id)
-    print("imdbId: ", self.imdb_id)
-    print("extroversion: ", self.extroversion)
-    print("neuroticism", self.neuroticism)
-    print("agreeableness: ", self.agreeableness)
-    print("conscientiousness: ", self.conscientiousness)
-    print("openess: ", self.openess)
+        if self.movielens_id == 'INVALID':
+            print("non_existant movie!\n")
+        else:
+            print("movielensId: ", self.movielens_id)
+            print("imdbId: ", self.imdb_id)
+            print("extroversion: ", self.extroversion)
+            print("neuroticism", self.neuroticism)
+            print("agreeableness: ", self.agreeableness)
+            print("conscientiousness: ", self.conscientiousness)
+            print("openess: ", self.openess)
+            print("user rating: ", self.rating)
 
-    print("movie genres: \n")
-    for genre in self.genre:
-        print(genre)
+            print("movie genres: \n")
+            for genre in self.genre:
+                print(genre)
 
 if __name__ == '__main__':
     
     # filme existente:
-    movie_test = Movie(115713)
-    print_movie(movie_test)
+    movie_test = Movie(115713, 4)
+    movie_test.print_movie()
+   
+
 
     # o filme abaixo não existe:
-    #movie_test = Movie(26674)
+    movie_test = Movie(26674, 2)
+    movie_test.print_movie()
--- generate_final_dataset.py
+++ generate_final_dataset.py
@@ -1,7 +1,8 @@
-from generating_temp_dataset import create_genre_columns
+from generating_temp_dataset import create_genre_columns, movielens_ids_of_final_db
 from pathlib import Path
 import pandas as pd
 import UserItem as UI
+import Movie
 
 def create_UserItem_teste():
 
@@ -49,10 +50,11 @@ def create_UserItem_teste():
 
                     )
 
+    user_and_movies_objects = []
     i = 0
     users = []
     for index, row in personality_data_df.iterrows():
-        #print (row)
+        #print ("row é: ", row)
 
         user_movies = []
 
@@ -72,6 +74,7 @@ def create_UserItem_teste():
         for movie in movie_items:
             user_movies.append(row[movie])
 
+        #print("user_movies :", user_movies)
         big_five = {}
         big_five['extraversion'] = row['extraversion']
         big_five['emotional_stability'] = row['emotional_stability']
@@ -79,20 +82,76 @@ def create_UserItem_teste():
         big_five['conscientiousness'] = row['conscientiousness']
         big_five['openess'] = row['openness']
 
-
         user = UI.UserItem(row['userid'], user_movies, big_five)
 
         users.append(user)
+        #print("users é: ", users)
+
+        movie_ratings = []
+        labels_ratings = ['predicted_rating_1',
+                            'predicted_rating_2',                                 
+                            'predicted_rating_3',  
+                            'predicted_rating_4', 
+                            'predicted_rating_5', 
+                            'predicted_rating_6', 
+                            'predicted_rating_7', 
+                            'predicted_rating_8', 
+                            'predicted_rating_9', 
+                            'predicted_rating_10', 
+                            'predicted_rating_11', 
+                            'predicted_rating_12'
+                        ]
+
+        for label in labels_ratings:
+            movie_ratings.append(row[label])
+
+        #print("movie_rating é: ", movie_ratings)
+
+        movie_and_rating = list(zip(user_movies, movie_ratings))
+        #print("movie and rating: ",movie_and_rating)
+
+        movies_objects = []
+
+        for items in movie_and_rating:
+
+            new_movie = Movie.Movie(items[0], items[1])
+            #print("new_movie é: \n", new_movie)
+            #Movie.print_movie(new_movie)
+            #new_movie.print_movie()
+            
+            if new_movie.movielens_id != 'INVALID':
+                movies_objects.append(new_movie)
+                #print("movielens Id desse filme é: ", new_movie.movielens_id)
+            
+        user_and_movies_objects.append((user, movies_objects))
+
         i+=1
         if i == 1:
             break
     
-    for new_user in users:
-        new_user.print_user()
-    #print(personality_data_df)
-#personality_csv_file = Path.cwd().joinpath('personality-isf2018', 'personality-data.csv')
-#movie_dat_file = Path.cwd().joinpath('tag-genome', 'movies.dat')
+    return user_and_movies_objects
+
+   
+def create_final_dataset_test(movie_user_list):
+
+    df_row_tuples = []
+
+    for objects in movie_user_list:
+        user = objects[0]
+        movie_list = objects[1]
+
+        for movie in movie_list:
+            row_tuple = (user, movie)
+            df_row_tuples.append(row_tuple)
+
+    for item in df_row_tuples:
+        print("linha: \n")
+        item[0].print_user()
+        item[1].print_movie()
+
 
 if __name__ == '__main__':
 
-    create_UserItem_teste()
+    movie_user_list = create_UserItem_teste()
+    #print("movie_user_list é: ", movie_user_list)
+    create_final_dataset_test(movie_user_list)
